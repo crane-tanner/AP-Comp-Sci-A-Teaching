8a2e70e6d873416b520595a0520e2ee81724b447
Tanner Crane <55415301+Cybernetic1001@users.noreply.github.com>
2025-03-16 21:16:25 -0700

    AP CSA Review Instructions

---
+++ AP_CSA_REVIEW/src/Main.java
@@ -0,0 +1,23 @@
+public class Main { // Review of some problems for the AP CSA Exam
+    public static void main(String[] args) {
+        /*
+        Primitive types
+        Writing Classes and Using Objects
+        BooleanExpressions and If statements
+        For/while loops
+        Arrays, ArrayLists, 2D Arrays
+        Inheritance
+        Recursion
+        Section 1: Multiple Choice - 40 Questions at 1 hour and 30 mins
+        50% of total score
+        Section 2: Free Response Questions
+        4 Questions with a 1.5 hour time limit
+        50% of total score
+        All code must be handwritten
+        Q1: Methods and control structures
+        Q2: Classes
+        Q3: Arrays/ArrayList
+        Q4: 2D Array
+         */
+    }
+}
